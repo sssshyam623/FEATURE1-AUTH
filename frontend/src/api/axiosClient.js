98360e19aa8d9164e201e981076c22581818b512
sssshyam623 <ssshyam623@gmail.com>
2026-06-20 19:19:47 +0530

    Update axiosClient.js

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../store/authStore";
 
 const axiosClient = axios.create({
-  baseURL: "/api",              // Vite proxy forwards this to localhost:5000
+  baseURL: "https://feature1-auth-backend.onrender.com/api",              // Vite proxy forwards this to localhost:5000
   headers: { "Content-Type": "application/json" },
 });
 
